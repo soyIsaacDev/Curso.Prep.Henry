f87ace6808cb881fff112036b36c3131e2583937
Isaac Borbon <borbonisaac@hotmail.com>
2021-07-16 12:41:43 -0700

    Tarea update

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -105,14 +105,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var grande = numeros[0];
   for (let i = 1; i < numeros.length; i++) {
-    numeros = numeros[i-1];
-    if (numeros > numeros[i] ) {
-      return numeros[i-1];
-    } else {
-      return numeros[i];
-    }
+    if ( numeros[i] > grande) {
+      grande = numeros [i];
+    }   
   }
+  return grande; 
 }
 
 
@@ -120,13 +119,28 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+      var multiplo = 1;
+      for (let i = 0; i < arguments.length; i++) {
+      multiplo = multiplo * arguments[i]
+    }
+  }  
+  return multiplo;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var cantidad = 0;
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 18) {
+      cantidad = cantidad + 1;
+    }
+  }
+  return cantidad
 }
 
 
@@ -134,8 +148,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí    D 1 L 2 M 3 MI 4 J 5 V6 S7
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return "Es fin de semana";
+  } else {
+    return "Es dia Laboral";
+  }
 } 
 
 
@@ -143,7 +161,14 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var rev = n.toString();
+  var primerDigito = rev.charAt(0);
+  if (primerDigito === "9") {
+    return true;
+  } else {
+    return false;
+  }
+ 
 }
 
 
@@ -151,7 +176,7 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+ 
 } 
 
 
